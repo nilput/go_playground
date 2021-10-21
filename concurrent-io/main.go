51922da1171729beabaaa5c1bd77215c9f81e70d
Turki Alsaleem <nilputs@gmail.com>
2021-10-21 04:40:24 +0300

    concurrent funcs

--- concurrent-io/main.go
+++ concurrent-io/main.go
@@ -3,8 +3,20 @@ package main
 import (
 	"concurrent_io/stdin_control"
 	"fmt"
+	"time"
 )
 
 func main() {
+	go func() {
+		i := 0
+		for {
+			time.Sleep(time.Millisecond * 500)
+			fmt.Println("Hey! ", i%10)
+			i++
+		}
+	}()
 	fmt.Println(stdin_control.PI)
+	for {
+		time.Sleep(1 * time.Second)
+	}
 }
